bb59d62068fe7d365677aa4e768f11a968228356
Sharif Olorin <sio@tesser.org>
2014-03-25 23:28:51 +1100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="esadmin",
+    version="0.1",
+    description="Elasticsearch administration module",
+    url="https://github.com/anchor/elasticsearchadmin",
+    packages=[
+        "esadmin",
+    ],
+)
