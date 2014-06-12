99bc332331a059c241997359840f1268293c36cf
Sharif Olorin <sio@tesser.org>
2014-06-12 16:25:55 +1000

    Add author and maintainer info to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,11 @@ setup(
     version="0.1",
     description="Elasticsearch administration module",
     url="https://github.com/anchor/elasticsearchadmin",
+    maintainer="Sharif Olorin",
+    maintainer_email="sio@tesser.org",
+    license="MIT",
+    author="Saj Goonatilleke",
+    author_email="sg@redu.cx",
     packages=[
         "esadmin",
     ],
